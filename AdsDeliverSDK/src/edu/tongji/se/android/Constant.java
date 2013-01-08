dd9ba7d3c96c679c90ce6e68173e4c7ac7f132ea
NLMartian <nl.martian@gmail.com>
2013-01-08 16:54:29 +0800

    修改了SDK常量：域名

--- AdsDeliverSDK/src/edu/tongji/se/android/Constant.java
+++ AdsDeliverSDK/src/edu/tongji/se/android/Constant.java
@@ -1,7 +1,7 @@
 package edu.tongji.se.android;
 
 public class Constant {
-	public static final String DOMAIN = "http://192.168.1.102:8080";
+	public static final String DOMAIN = "http://alfa7055.vicp.cc";
 	
 	public static final String AD_IMAGE_PATH_KEY = "adImagePathKey";
 	public static final String AD_CONTENT_KEY = "adContentKey";
